456a054023b97d45c64a9b750438775f90d21ad0
FathimaSijilaBanu <sijila.b@codilar.com>
2023-06-12 13:45:55 +0530

    made some changes

--- Pincode/Block/Availability.php
+++ Pincode/Block/Availability.php
@@ -6,28 +6,24 @@ use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\RequestInterface;
 use Codilar\Pincode\Api\PincodeRepositoryInterface;
-use Magento\Framework\App\ResourceConnection;
 
 class Availability extends Template
 {
     protected $productRepository;
     protected $request;
     protected $pincodeRepository;
-    protected $connection;
 
     public function __construct(
         Template\Context $context,
         ProductRepositoryInterface $productRepository,
         RequestInterface $request,
         PincodeRepositoryInterface $pincodeRepository,
-        ResourceConnection $resource,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->productRepository = $productRepository;
         $this->request = $request;
         $this->pincodeRepository = $pincodeRepository;
-        $this->connection = $resource->getConnection();
     }
 
     public function getCurrentProduct()
